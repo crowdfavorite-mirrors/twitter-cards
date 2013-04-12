eac00e2ff139a73374516cd085771741846f9478
Steven Mathias <steve@crowdfavorite.com>
2013-04-12 14:51:48 -0600

    Updated to support galleries, added filtering to allow themes or other plugins to override some of the twitter card selection functionality.

--- twitter-cards.php
+++ twitter-cards.php
@@ -33,22 +33,68 @@ class Twitter_Cards {
 	 */
 	public static function markup() {
 		global $post;
-		if ( ! ( isset( $post ) && isset( $post->ID ) ) )
-			return;
-
-		setup_postdata( $post );
-
-		$post_id = absint( $post->ID );
-		if ( ! $post_id )
-			return;
-
-		$post_type = get_post_type( $post );
-		if ( ! $post_type )
-			return;
-		$post_format = get_post_format( $post_id );
+		$card_type = apply_filters('twitter_cards_cardtype', 'summary', $post);
 
 		if ( ! class_exists( 'Twitter_Card_WP' ) )
 			require_once( dirname(__FILE__) . '/class-twitter-card-wp.php' );
+		
+		$card = apply_filters('twitter_cards_carddata', new Twitter_Card_WP($card_type), $card_type, $post);
+
+		if ( apply_filters( 'twitter_cards_htmlxml', 'html' ) === 'xml' )
+			echo $card->asXML();
+		else
+			echo $card->asHTML();
+	}
+	
+	public static function setup_card_data($card, $card_type, $post) {
+		global $post;
+		
+		if ( ! ( isset( $post ) && isset( $post->ID ) ) )
+			return $type;
+		
+		setup_postdata($post);
+		
+		$post_type = get_post_type($post);
+		
+		$card->setURL( get_permalink( $post_id ) );
+		if ( post_type_supports( $post_type, 'title' ) ) {
+			$card->setTitle( get_the_title() );
+		}
+		if ( post_type_supports( $post_type, 'excerpt' ) ) {
+			// one line, no HTML
+			$description = self::make_description( $post );
+			if ( $description )
+				$card->setDescription( $description );
+			unset( $description );
+		}
+		
+		$post_thumbnail_url = false;
+		if ( post_type_supports( $post_type, 'thumbnail' ) && function_exists( 'has_post_thumbnail' ) && has_post_thumbnail() )
+			list( $post_thumbnail_url, $post_thumbnail_width, $post_thumbnail_height ) = wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), 'full' );
+
+		if ( $post_thumbnail_url )
+			$card->setImage( $post_thumbnail_url, $post_thumbnail_width, $post_thumbnail_height );
+		
+		return $card;
+	}
+	
+	public static function card_type($type, $post) {
+		global $post;
+		
+		if ( ! ( isset( $post ) && isset( $post->ID ) ) )
+			return $type;
+		
+		setup_postdata($post);
+
+		$post_id = absint( $post->ID );
+		if ( ! $post_id )
+			return $type;
+
+		$post_type = get_post_type( $post );
+		if ( ! $post_type )
+			return $type;
+			
+		$post_format = get_post_format( $post_id );
 
 		// does current post type and the current theme support post thumbnails?
 		$post_thumbnail_url = false;
@@ -58,27 +104,9 @@ class Twitter_Cards {
 		// treat as photo card if post type image
 		// why not also check for post format video? No real easy way in WordPress Core to ask for an iframe version of the video embedded in your post, meaning you'll have to tap into a filter and may as well set the card value alongside required attributes such as player
 		if ( $post_format === 'image' && $post_thumbnail_url && $post_thumbnail_width >= 280 && $post_thumbnail_height >= 150 )
-			$card = new Twitter_Card_WP( 'photo' );
-		else
-			$card = new Twitter_Card_WP();
-		$card->setURL( get_permalink( $post_id ) );
-		if ( post_type_supports( $post_type, 'title' ) )
-			$card->setTitle( get_the_title() );
-		if ( post_type_supports( $post_type, 'excerpt' ) ) {
-			// one line, no HTML
-			$description = self::make_description( $post );
-			if ( $description )
-				$card->setDescription( $description );
-			unset( $description );
-		}
-
-		if ( $post_thumbnail_url )
-			$card->setImage( $post_thumbnail_url, $post_thumbnail_width, $post_thumbnail_height );
-
-		if ( apply_filters( 'twitter_cards_htmlxml', 'html' ) === 'xml' )
-			echo $card->asXML();
-		else
-			echo $card->asHTML();
+			$type = 'photo';
+		
+		return $type;
 	}
 
 	/**
@@ -124,5 +152,8 @@ class Twitter_Cards {
 	}
 }
 add_action( 'wp', 'Twitter_Cards::init' );
+add_filter('twitter_cards_cardtype', 'Twitter_Cards::card_type', 10, 2);
+add_filter('twitter_cards_carddata', 'Twitter_Cards::setup_card_data', 10, 3);
+
 endif;
 ?>
